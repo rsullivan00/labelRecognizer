0f3d6fdafd5d0b27ab9b739f86c1fb53ac4d7d83
Rick Sullivan <rick@ricksullivan.net>
2014-11-05 23:33:50 -0800

    Python you are so fun to write

--- tesseractTesting/processTestImages.py
+++ tesseractTesting/processTestImages.py
@@ -2,12 +2,35 @@ import os
 import glob
 import Image
 import pytesseract
+import cv2
+import re
 
-os.chdir('../labels/')
-for file in glob.glob('*.jpg'):
-    print file
-    output = pytesseract.image_to_string(Image.open(file))
-    outFileName = file[0:len(file)-4]
-    outFile = open('../tesseractTesting/outputs/' + outFileName + '.txt', 'w')
-    outFile.write(output)
+def writeFile(directory, fileName, content):
+    outFile = open('outputs/' + directory + '/' + fileName + '.txt', 'w')
+    outFile.write(content)
     outFile.close()
+
+# Setup regex for stripping file names
+p = re.compile('[0-9]+.jpg')
+    
+# Use images in the labels directory
+for file in glob.glob('../labels/*.jpg'):
+    fileName = p.findall(file)[0]
+    print ("Proccessing " + fileName)
+    outFileName = fileName[0:len(fileName)-4]
+    # Get default text recognition 
+    output = pytesseract.image_to_string(Image.open(file))
+
+    # Write default text output to 'outputs/default' directory
+    writeFile('default', outFileName, output)
+
+    # Open image as black and white
+    bw = cv2.imread(file, 0)
+
+    # Apply adaptive thresholding to images
+    thresh = cv2.adaptiveThreshold(bw, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY, 11, 2)
+
+    # Get text recognition from thresholded images
+    output = pytesseract.image_to_string(Image.fromarray(bw))
+    writeFile('bw', outFileName, output)
