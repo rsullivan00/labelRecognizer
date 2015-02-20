cd490c4203acb7106eaa9b5c8b2916e523c60635
Rick Sullivan <rick@ricksullivan.net>
2015-02-19 18:01:28 -0800

    Moved label python class out of other script

--- generateJSON.py
+++ generateJSON.py
@@ -2,24 +2,7 @@ import sys
 import csv
 import os
 import json
-
-class Label:
-    def __init__(self, attr_list):
-        attrs = ['name', 'calories', 'total_fat', 'saturated_fat', 'trans_fat', 'poly_fat', 'mono_fat', 'cholesterol', 'sodium', 'potassium', 'carbohydrates', 'fiber', 'sugars', 'protein', 'skewed', 'not_bw', 'curved', 'wrinkled', 'shadowed', 'blurry']
-        for i, attr in enumerate(attrs):
-            # Final 6 attributes are true/false
-            true_false_attrs = attrs[-6:]
-            if attr in true_false_attrs:
-                def processTF(tfstring):
-                    if tfstring == "Yes":
-                        return True
-                    else:
-                        return False
-                attr_list[i] = processTF(attr_list[i])
-            elif attr_list[i] == "":
-                attr_list[i] = None
-
-            setattr(self, attr, attr_list[i])
+from label import Label
 
 def processCSV(filename="label_data.csv", outputdir="db"):
     with open(filename) as csvfile:
@@ -41,6 +24,8 @@ def processCSV(filename="label_data.csv", outputdir="db"):
             outfile.close()
             print(label.name)
 
+# Runs on running this script
+# Usage: python3 generateJSON.py [input.csv] [outputDir]
 if len(sys.argv) > 2:
     processCSV(sys.argv[1], sys.argv[2])
 elif len(sys.argv) > 1:
---
+++ label.py
@@ -0,0 +1,23 @@
+class Label:
+    """Label class contains all nutritional information corresponding to a food label."""
+    def __init__(self, attr_list=None):
+        attrs = ['name', 'calories', 'total_fat', 'saturated_fat', 'trans_fat', 'poly_fat', 'mono_fat', 'cholesterol', 'sodium', 'potassium', 'carbohydrates', 'fiber', 'sugars', 'protein', 'skewed', 'not_bw', 'curved', 'wrinkled', 'shadowed', 'blurry']
+        if attr_list is None:
+            attr_list = [None]*len(attrs)
+
+        for i, attr in enumerate(attrs):
+            # Final 6 attributes are true/false
+            true_false_attrs = attrs[-6:]
+            if attr in true_false_attrs:
+
+                def processTF(tfstring):
+                    if tfstring == "Yes":
+                        return True
+                    else:
+                        return False
+
+                attr_list[i] = processTF(attr_list[i])
+            elif attr_list[i] == "":
+                attr_list[i] = None
+
+            setattr(self, attr, attr_list[i])
