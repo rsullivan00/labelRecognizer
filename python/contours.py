00b641e3fdc36727deb76dce817171457362ebbb
Rick Sullivan <rick@ricksullivan.net>
2015-03-04 22:36:31 -0800

    Python testing

--- python/contours.py
+++ python/contours.py
@@ -3,78 +3,96 @@ import sys
 import os
 import math
 import random
+import numpy as np
 
 def contour(imagepath, show=True):
-	im = cv2.imread(imagepath)
-	im_x = len(im)
-	im_y = len(im[0])
-	im_size = im_x * im_y
-	imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-	ret,thresh = cv2.threshold(imgray,127,255,0)
-#	thresh = cv2.adaptiveThreshold(imgray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,21,0)
-	image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    im = cv2.imread(imagepath)
+    im_x = len(im)
+    im_y = len(im[0])
+    im_size = im_x * im_y
+    imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+#    imgray = cv2.medianBlur(imgray,7)
+    ret,thresh = cv2.threshold(imgray,127,255,0)
+#    thresh = cv2.adaptiveThreshold(imgray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,21,0)
+    image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
 
-	# Filter based on contour size
-	large_contours = []
-	contour_area_thresh = im_size/8
-	for cnt in contours:
-		area = cv2.contourArea(cnt)
-		if area > contour_area_thresh:
-			large_contours.append(cnt)
+    # Filter based on contour size
+    large_contours = []
+    contour_area_thresh = im_size/8
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > contour_area_thresh:
+            large_contours.append(cnt)
 
-	print('%d large contours found' % len(large_contours))
+    print('%d large contours found' % len(large_contours))
 
-	# Now filter based on the location of the contour's centroid
-	possible_label_contours = []
-	cx_im = im_x/2
-	cy_im = im_y/2
-	centroid_distance_thresh = im_size/2000
-	print('Euclidean thresh: %d' % centroid_distance_thresh)
-	for cnt in large_contours:
-		# Moments of the contour
-		M = cv2.moments(cnt)
-		# Find the centroid
-		cx = int(M['m10']/M['m00'])
-		cy = int(M['m01']/M['m00'])
-		# Euclidean distance from center
-		dist = math.sqrt((cx-im_x)**2 + (cy-im_y)**2)
-		if dist < centroid_distance_thresh:
-			possible_label_contours.append(cnt)
-			print('Euclidean distance: %d' % dist)
-	
-	removed = len(large_contours) - len(possible_label_contours)
-	if removed > 0:
-		print('%d contours removed for location' % removed)
+    # Now filter based on the location of the contour's centroid
+    possible_label_contours = []
+    cx_im = im_x/2
+    cy_im = im_y/2
+    centroid_distance_thresh = im_size/2000
+    print('Euclidean thresh: %d' % centroid_distance_thresh)
+    for cnt in large_contours:
+        # Moments of the contour
+        M = cv2.moments(cnt)
+        # Find the centroid
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        # Euclidean distance from center
+        dist = math.sqrt((cx-im_x)**2 + (cy-im_y)**2)
+        if dist < centroid_distance_thresh:
+            possible_label_contours.append(cnt)
+            print('Euclidean distance: %d' % dist)
 
-	label_contours = []
-	# Find contour in the middle of the image
-	for cnt in possible_label_contours:
-		# Check if contour contains the center of the image
-		if cv2.pointPolygonTest(cnt, (cx_im, cy_im), False) > 0:
-    			label_contours.append(cnt)
+    removed = len(large_contours) - len(possible_label_contours)
+    if removed > 0:
+        print('%d contours removed for location' % removed)
 
-	removed = len(possible_label_contours) - len(label_contours)
-	if removed > 0:
-		print('%d contours removed after center check' % removed)
+    label_contours = []
+    # Find contour in the middle of the image
+    for cnt in possible_label_contours:
+        # Check if contour contains the center of the image
+        if cv2.pointPolygonTest(cnt, (cx_im, cy_im), False) > 0:
+            label_contours.append(cnt)
 
-	print('%d contours found' % len(label_contours))
+    removed = len(possible_label_contours) - len(label_contours)
+    if removed > 0:
+        print('%d contours removed after center check' % removed)
 
-	# Draw remaining contours on original image
-	if show:
-		for i in range(len(label_contours)):
-			cv2.drawContours(im, label_contours, i, (random.randint(0,255),random.randint(0,255),random.randint(0,255)), -5)
-		cv2.namedWindow('contour', cv2.WINDOW_NORMAL)
-		cv2.imshow('contour', im)
-		cv2.waitKey()
+    print('%d contours found' % len(label_contours))
+
+    if len(label_contours) == 0:
+        return
+
+    contour = cv2.approxPolyDP(label_contours[0], 10, True)
+    rect = cv2.minAreaRect(contour)
+    center = rect[0]
+    size = (int(rect[1][0]), int(rect[1][1]))
+    angle = rect[2]
+#    map_matrix_cv = cv2.fromarray([[np.cos(angle), -np.sin(angle), center[0]], 
+#        [np.sin(angle), np.cos(angle), center[1]]])
+    rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+    cv2.warpAffine(thresh, rotation_matrix, dsize=(len(thresh), len(thresh[0])), dst=thresh)
+    pix = cv2.getRectSubPix(thresh, size, center)
+    print(len(contour))
+
+    # Draw contour on original image
+    if show:
+        cv2.medianBlur(pix, 3)
+        edges = cv2.Canny(pix,70,200,5)
+        cv2.drawContours(im, [], -1, (random.randint(0,255),random.randint(0,255),random.randint(0,255)), -5)
+        cv2.namedWindow('contour', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('contour', 800, 800)
+        cv2.imshow('contour', edges)
+        cv2.waitKey()
 
 
 def processAllImages(dirpath):
-	for file in os.listdir(dirpath):
-		print(file)
-		if file.endswith(".jpg"):
-			contour(os.path.join(dirpath, file), show=False)
-	
+    for file in os.listdir(dirpath):
+        if file.endswith(".jpg"):
+            contour(os.path.join(dirpath, file), show=True)
 
-#processAllImages('../labels/')
-contour(sys.argv[1])
+
+processAllImages('../labels/')
+#contour(sys.argv[1])
